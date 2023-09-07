cba5f336d76b4bf98f5f14dbf6ce4ef947376e65
tracy-78898 <tdurha3@live.spcollege.edu>
2023-09-07 07:03:18 -0500

    successful build

--- app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.raywenderlich.listmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.raywenderlich.listmaker.R.layout.fragment_main
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.raywenderlich.listmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+
         }
-    }
+        binding.fabButton.setOnClickListener{
+            showCreateListDialog()
+        }
+    }private fun showCreateListDialog() {
+        // 1
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+        // 2
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+            listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+        // 3
+        builder.setPositiveButton(positiveButtonTitle){
+                dialog,_ -> dialog.dismiss()
+        }
+        // 4
+        builder.create().show()}
 }

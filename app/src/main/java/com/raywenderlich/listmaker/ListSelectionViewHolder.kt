4e29e9bf1deedb4f444b15c91bcc19fe42093b34
tracy-78898 <tdurha3@live.spcollege.edu>
2023-09-07 03:11:20 -0500

    will not run

--- app/src/main/java/com/raywenderlich/listmaker/ListSelectionViewHolder.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListSelectionViewHolder.kt
@@ -2,8 +2,4 @@ package com.raywenderlich.listmaker
 import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.listmaker.databinding.ListSelectionViewHolderBinding
 
-class ListSelectionViewHolder(val binding:
-                              ListSelectionViewHolderBinding
-) :
-    RecyclerView.ViewHolder(binding.root)  {
-}
+class ListSelectionViewHolder(val binding:ListSelectionViewHolderBinding):RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import com.raywenderlich.listmaker.R.layout.fragment_main
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(fragment_main)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+        }
     }
 }
